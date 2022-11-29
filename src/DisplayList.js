2026ae659cbb43685c1366a412e8a06554e2d165
softbear888 <grace_wan+cs1300@brown.edu>
2022-11-29 01:00:11 -0500

    Just removing DisplayList

--- src/DisplayList.js
+++
@@ -1,7 +0,0 @@
-import FrogCard from './FrogCard';
-import { ListItemButton, ListItemText, List } from '@mui/material';
-import Benny from './images/Benny.jpg';
-
-export default function DisplayList(props) {
-
-}
